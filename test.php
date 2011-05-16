81ddd4386fab833a94ee8101e4fc105932d9306c
Tim Rogers <tim.rogers@carnochanrogers.co.uk>
2011-05-16 22:14:58 +0100

    The example code in test.php is pretty awesome now.

--- test.php
+++ test.php
@@ -1,16 +1,32 @@
 <?php
 include("Taskforce.inc.php");
 
-#Add your Taskforce username and password
+#Add your Taskforce username and password here, which is then put into the object below
 $username = "";
 $password = "";
 
 
+#Making a Taskforce object using the username and password from above
 $object = new Taskforce($username, $password);
-$user = $object->createList("Test from API");
-if ($user != false) {
-var_dump($user);
+
+
+$list = $object->createList("A list I'm going to create");
+if ($list != false) {
+/* The list was created fine - $user will contain the JSON returned */
 } else {
-die("Error.");
+/* Something went wrong, who knows what though. */
+}
+
+/*
+		So yeah. You pretty much follow the pattern from createList() above with every method. Just make sure you read README.md for any of the weird exceptions, 
+		Check Taskforce.inc.php or the API documentation at http://www.taskforceapp.com/api to find out all the parameters and how everything works.
+*/
+
+#The createUser() method is static, and is not called from within a Taskforce object
+$new = Taskforce::createUser("an_email_address@email.com", "afasrasljrlay8q3y78");
+if ($new == true) {
+	# The user was created okay - this method is an exception to the usual behaviour, in that it returns true if successful, rather than the JSON response
+} else {
+	# Couldn't make the user. The result was false.
 }
 ?>
